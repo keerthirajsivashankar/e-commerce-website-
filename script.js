b3e762b47d71dc30fd1daf439e6b438d5da5e9c3
keerthirajsivashankar <keerthirajsivashankar@gmail.com>
2025-10-06 20:34:00 +0530

    completed some carousel works

--- script.js
+++ script.js
@@ -50,6 +50,8 @@ function resetInterval() {
   slideInterval = setInterval(nextSlide, 4000);
 }
 
+// Shoe carousel functionality
+
 const container = document.querySelector(".shoe-carousel-container");
 const sprevBtn = document.querySelector(".shoe-carousel-btn.prev");
 const snextBtn = document.querySelector(".shoe-carousel-btn.next");
@@ -66,4 +68,37 @@ snextBtn.addEventListener("click", () => {
 // Prev button
 sprevBtn.addEventListener("click", () => {
   container.scrollBy({ left: -cardWidth, behavior: "smooth" });
+});
+
+//watch carousel functionality
+const wcontainer = document.querySelector(".watch-carousel-container");
+const wprevBtn = document.querySelector(".watch-carousel-btn.prev");
+const wnextBtn = document.querySelector(".watch-carousel-btn.next");
+
+// get one card width (includes margin if any)
+const wcard = document.querySelector(".shoe-card");
+const wcardWidth = wcard.offsetWidth + 20; // add gap if you set margin
+// Next button
+wnextBtn.addEventListener("click", () => {
+  wcontainer.scrollBy({ left: wcardWidth, behavior: "smooth" });
+});
+// Prev button
+wprevBtn.addEventListener("click", () => {
+  wcontainer.scrollBy({ left: -wcardWidth, behavior: "smooth" });
+});
+
+// glass carousel functionality
+const gcontainer = document.querySelector(".glass-carousel-container");
+const gprevBtn = document.querySelector(".glass-carousel-btn.prev");
+const gnextBtn = document.querySelector(".glass-carousel-btn.next");
+// get one card width (includes margin if any)
+const gcard = document.querySelector(".shoe-card");
+const gcardWidth = gcard.offsetWidth + 20; // add gap if you set margin
+// Next button
+gnextBtn.addEventListener("click", () => {
+  gcontainer.scrollBy({ left: gcardWidth, behavior: "smooth" });
+});
+// Prev button
+gprevBtn.addEventListener("click", () => {
+  gcontainer.scrollBy({ left: -gcardWidth, behavior: "smooth" });
 });
